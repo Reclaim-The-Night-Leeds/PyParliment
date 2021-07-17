86804d71d4f1c80715e29746b1d99b6881f8bad6
George Sykes <el18gs@leeds.ac.uk>
2021-07-18 00:06:32 +0100

    Added methods for anunciator system

---
+++ src/pyparliment/now.py
@@ -0,0 +1,63 @@
+import requests
+import json
+from datetime import datetime, timedelta
+from tqdm.auto import trange
+
+
+def get_current(annunciator="CommonsMain"):
+    end_point = "Message/message/" + annunciator + "/current"
+    baseURL = "https://now-api.parliament.uk/api/"
+    url = baseURL + end_point
+    response = requests.request("GET", url)
+    if response.status_code == 200:
+        json_response = json.loads(response.text)
+    else:
+        raise NothingToSay
+
+    return json_response
+
+
+def get_by_datetime(date, annunciator="CommonsMain"):
+    if type(date) not in [datetime, str]:
+        raise ValueError("parameter 'date' must be of type datetime.datetime or string")
+    elif type(date) == str:
+        date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    end_point = (
+        "Message/message/" + annunciator + "/" + date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    )
+    baseURL = "https://now-api.parliament.uk/api/"
+    url = baseURL + end_point
+    print(url)
+    response = requests.request("GET", url)
+    json_response = json.loads(response.text)
+
+    return json_response
+
+
+def get_between_datetime(date_start, date_end, annunciator="CommonsMain"):
+    start = datetime.strptime(date_start, "%Y-%m-%dT%H:%M:%S.%fZ")
+    end = datetime.strptime(date_end, "%Y-%m-%dT%H:%M:%S.%fZ")
+    diff = end - start
+
+    added_ids = []
+
+    for i in trange(0, int(diff.total_seconds() / 60) + 1):
+        time = start + timedelta(minutes=i)
+        current = get_by_datetime(time)
+        output = "testing/" + str(current["id"]) + ".json"
+
+        for slide in current["slides"]:
+            if (current["id"] not in added_ids) and (slide["soundToPlay"] == 1):
+                added_ids.append(current["id"])
+                output = "testing/" + str(current["id"]) + ".json"
+                with open(output, "w") as f:
+                    f.write(json.dumps(current, indent=4, sort_keys=True))
+
+
+class NothingToSay(Exception):
+    """Raised when there of the requested type being displayed on the annunciator"""
+
+    def __init__(self):
+        # Call the base class constructor with the parameters it needs
+        super().__init__("The annunciator is not displaying anything at the moment")
